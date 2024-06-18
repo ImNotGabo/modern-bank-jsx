d95a9a423bff9e79254a1fabc39b3552354e5b7a
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-06-18 09:42:34 -0400

    Hyperlink

--- src/constants/index.js
+++ src/constants/index.js
@@ -103,23 +103,23 @@ export const footerLinks = [
     links: [
       {
         name: 'Content',
-        link: 'https://www.hoobank.com/content/',
+        link: 'https://www.iabank.com/content/',
       },
       {
         name: 'How it Works',
-        link: 'https://www.hoobank.com/how-it-works/',
+        link: 'https://www.iabank.com/how-it-works/',
       },
       {
         name: 'Create',
-        link: 'https://www.hoobank.com/create/',
+        link: 'https://www.iabank.com/create/',
       },
       {
         name: 'Explore',
-        link: 'https://www.hoobank.com/explore/',
+        link: 'https://www.iabank.com/explore/',
       },
       {
         name: 'Terms & Services',
-        link: 'https://www.hoobank.com/terms-and-services/',
+        link: 'https://www.iabank.com/terms-and-services/',
       },
     ],
   },
@@ -128,23 +128,23 @@ export const footerLinks = [
     links: [
       {
         name: 'Help Center',
-        link: 'https://www.hoobank.com/help-center/',
+        link: 'https://www.iabank.com/help-center/',
       },
       {
         name: 'Partners',
-        link: 'https://www.hoobank.com/partners/',
+        link: 'https://www.iabank.com/partners/',
       },
       {
         name: 'Suggestions',
-        link: 'https://www.hoobank.com/suggestions/',
+        link: 'https://www.iabank.com/suggestions/',
       },
       {
         name: 'Blog',
-        link: 'https://www.hoobank.com/blog/',
+        link: 'https://www.iabank.com/blog/',
       },
       {
         name: 'Newsletters',
-        link: 'https://www.hoobank.com/newsletters/',
+        link: 'https://www.iabank.com/newsletters/',
       },
     ],
   },
@@ -153,11 +153,11 @@ export const footerLinks = [
     links: [
       {
         name: 'Our Partner',
-        link: 'https://www.hoobank.com/our-partner/',
+        link: 'https://www.iabank.com/our-partner/',
       },
       {
         name: 'Become a Partner',
-        link: 'https://www.hoobank.com/become-a-partner/',
+        link: 'https://www.iabank.com/become-a-partner/',
       },
     ],
   },
