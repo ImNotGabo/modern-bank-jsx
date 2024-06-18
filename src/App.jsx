68c87cbcaa6c1f9d828838f4442526fae759f159
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-06-18 09:51:07 -0400

    Fixed Semanthic

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,11 @@ export default function App() {
   return (
     <>
       <header className='bg-primary w-full overflow-hidden'>
-        <nav className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
           <div className={styles.boxWidth}>
             <Navbar />
           </div>
-        </nav>
+        </div>
       </header>
 
       <section className={`bg-primary ${styles.flexStart}`}>
