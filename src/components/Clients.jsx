34bdb3cea7656d0e9ffe6618f24dab926cefe9ca
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-06-23 14:25:22 -0400

    Clients done

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,5 +1,18 @@
+import { clients } from '../constants';
+import styles from '../styles';
+
 const Clients = () => {
-  return <div>Client</div>;
+  return (
+    <section className={`${styles.flexCenter} my-4`} id='clients'>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div className={`flex-1  ${styles.flexCenter} sm:min-w-[192px] min-w-[192px]`} key={client.id}>
+            <img src={client.logo} alt='client' className='sm:w-[192px] w-[200px] object-contain' />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
