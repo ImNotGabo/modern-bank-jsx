17f2e69b3a05f2466785724435097fe03d798597
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-06-23 13:23:26 -0400

    CardDeal done

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,5 +1,24 @@
+import { card } from '../assets';
+import styles, { layout } from '../styles';
+import Button from './Button';
+
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className='sm:block hidden' /> in few easy steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}> You can find a better card deal in few easy steps.</p>
+
+        <Button style='mt-10' />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt='card' className='w-[100%] h-[100%]' />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
