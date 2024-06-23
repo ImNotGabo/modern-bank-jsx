878adf9068e07f11b8b881d9359c9e9c6724a07b
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-06-23 18:14:21 -0400

    Clients done

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -6,8 +6,8 @@ const Clients = () => {
     <section className={`${styles.flexCenter} my-4`} id='clients'>
       <div className={`${styles.flexCenter} flex-wrap w-full`}>
         {clients.map((client) => (
-          <div className={`flex-1  ${styles.flexCenter} sm:min-w-[192px] min-w-[192px]`} key={client.id}>
-            <img src={client.logo} alt='client' className='sm:w-[192px] w-[200px] object-contain' />
+          <div className={`flex-1  ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`} key={client.id}>
+            <img src={client.logo} alt='client' className='sm:w-[192px] w-[150px] object-contain' />
           </div>
         ))}
       </div>
