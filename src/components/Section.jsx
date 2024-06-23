cf05dde182acdff97d5847cb5fb30f77465a0e68
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-06-23 19:07:29 -0400

    Section fixed

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,12 +1,10 @@
 import styles from '../styles';
 /* eslint-disable react/prop-types */
 
-const Section = ({ content }) => {
-  return (
-    <section className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={styles.boxWidth}>{content}</div>
-    </section>
-  );
-};
+const Section = ({ content }) => (
+  <section className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={styles.boxWidth}>{content}</div>
+  </section>
+);
 
 export default Section;
