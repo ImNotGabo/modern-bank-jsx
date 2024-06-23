a3100937d259b1fa0ee34244dfd0deef5d5ecc28
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-06-23 19:53:50 -0400

    TODO

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,16 +1,16 @@
-import styles from '../styles';
 import { feedback } from '../constants';
-import FeedbacCard from './FeedbackCard';
+import styles from '../styles';
+import FeedbackCard from './FeedbackCard';
 
 const Testimonials = () => (
-  <section className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`} id='clients'>
-    <div className='absolute z-[0] w-[50%] h-[60%] -right-[50%] rounded-full blue__gradient' />
-    <div className={`w-full flex flex-col justify-between items-center md:flex-row sm:mb-16 mb-6 relative z-[1]`}>
-      <h1 className={styles.heading2}>
-        What people are <br className='sm:block hidden' />
-        saying about us
-      </h1>
+  <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
+    {/* TODO: Fix background gradient*/}
+    {/* <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40' /> */}
 
+    <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
+      <h2 className={styles.heading2}>
+        What People are <br className='sm:block hidden' /> saying about us
+      </h2>
       <div className='w-full md:mt-0 mt-6'>
         <p className={`${styles.paragraph} text-left max-w-[450px]`}>
           Everything you need to accept card payments and grow your business anywhere on the planet.
@@ -18,9 +18,9 @@ const Testimonials = () => (
       </div>
     </div>
 
-    <div className='w-full flex flex-wrap sm:justify-start justify-center feedback-container relative z-[1]'>
+    <div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
       {feedback.map((card) => (
-        <FeedbacCard key={card.id} {...card} />
+        <FeedbackCard key={card.id} {...card} />
       ))}
     </div>
   </section>
