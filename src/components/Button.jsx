efd1ea3cc62fc861b2a6a2d16833917aa7c5148a
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-06-23 12:49:28 -0400

    Button fixed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ style }) => {
   return (
     <button
       type='button'
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary text-[18px] outline-none  ${style}`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary text-[18px] outline-none  ${style} rounded-lg`}
     >
       Get started
     </button>
